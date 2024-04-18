7fda3c356f094f493855cef79c215c71ee78569f
ciraganenicole <ciraganenicole@gmail.com>
2024-04-18 09:30:52 +0200

    Add price

--- src/utils/prices.ts
+++ src/utils/prices.ts
@@ -4,23 +4,23 @@ export const prices = [
     description: '2 Fiches',
   },
   {
-    price: '1$',
-    description: '2 Fiches',
+    price: '2.99$',
+    description: '6 Fiches',
   },
   {
-    price: '1$',
-    description: '2 Fiches',
+    price: '5.99$',
+    description: '13 Fiches',
   },
   {
-    price: '1$',
-    description: '2 Fiches',
+    price: '11.99$',
+    description: '25 Fiches',
   },
   {
-    price: '1$',
-    description: '2 Fiches',
+    price: '24.99$',
+    description: '52 Fiches',
   },
   {
-    price: '1$',
-    description: '2 Fiches',
+    price: '49.99$',
+    description: '110 Fiches',
   },
 ];
