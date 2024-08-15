9cdbc6f0adfa83b539072de3feb7dd50f7b36c26
lemoissonM <murhulametre@gmail.com>
2024-08-15 04:46:32 +0300

    Add usage and privacy policy

---
+++ src/pages/privacy-policy.tsx
@@ -0,0 +1,168 @@
+import Head from 'next/head';
+import React from 'react';
+
+export default function PrivacyPolicy() {
+  return (
+    <div className="text-justify">
+      <Head>
+        <title>Politique de confidentialité de Dug Assistant</title>
+        <meta
+          name="description"
+          content="Politique de confidentialité de Dug Assistant"
+        />
+      </Head>
+      <div className="bg-primary-400 pb-8 pt-16 text-white ">
+        <div className="mx-auto max-w-[1000px] px-10">
+          <h1 className="mb-5 text-3xl font-bold text-white">
+            Politique de confidentialité de Dug Assistant
+          </h1>
+        </div>
+      </div>
+      <div className="mx-auto max-w-[1000px] p-10">
+        <p className="mt-5">
+          Cette politique de confidentialité décrit comment Dug Assistant («
+          nous », « notre » ou « nos ») collecte, utilise et divulgue vos
+          informations personnelles lorsque vous utilisez notre site Web,
+          Dugssistant.com (le « Site Web »), et les services qui y sont proposés
+          (le « Service »).
+        </p>
+
+        <h2 className="my-2 text-xl font-semibold text-primary-400">
+          Informations que nous recueillons
+        </h2>
+        <p>
+          Nous collectons plusieurs types d&apos;informations à diverses fins
+          pour améliorer notre service à votre égard. Nous pouvons collecter :
+        </p>
+        <ul className="my-3 py-4">
+          <li>
+            <b>1. Informations de compte :</b> Lorsque vous créez un compte,
+            nous collectons votre adresse e-mail, votre numéro de téléphone
+            (facultatif), votre mot de passe (haché et salé), votre prénom et
+            votre nom de famille. Ces informations sont utilisées pour
+            l&apos;authentification, l&apos;identification et pour vous
+            contacter au sujet de votre compte.
+          </li>
+          <li>
+            <b>2. Informations de génération de leçons :</b> Lorsque vous
+            utilisez le Service pour générer des leçons, vous pouvez fournir des
+            informations telles que le nom de la classe, le niveau, le cours, le
+            titre de la leçon, le titre de la leçon précédente et les ressources
+            (texte, etc.). Ces informations servent uniquement à générer des
+            leçons personnalisées pour vous.
+          </li>
+          <li>
+            <b>3. Informations de paiement :</b> Si vous choisissez de payer le
+            Service par mobile money, nous pouvons collecter votre numéro de
+            téléphone pour traiter le paiement en toute sécurité.
+          </li>
+        </ul>
+
+        <h2 className="my-2 text-xl font-semibold text-primary-400">
+          Comment nous utilisons vos informations
+        </h2>
+        <p>
+          Nous utilisons les informations que nous collectons de diverses
+          manières, notamment :
+        </p>
+        <ol className="py-4">
+          <li>1. Pour fournir et exploiter le Service</li>
+          <li>2. Pour créer et gérer votre compte</li>
+          <li>3. Pour vous identifier et vous authentifier</li>
+          <li>4. Pour traiter les paiements (si applicable)</li>
+          <li>
+            5. Pour améliorer le Service et développer de nouvelles
+            fonctionnalités
+          </li>
+          <li>
+            6. Pour communiquer avec vous au sujet du Service, y compris
+            l&apos;envoi de mises à jour, d&apos;offres promotionnelles et
+            d&apos;informations d&apos;assistance (avec votre consentement)
+          </li>
+        </ol>
+
+        <h2 className="my-2 text-xl font-semibold text-primary-400">
+          Conservation des données
+        </h2>
+        <p>
+          Nous conserverons vos informations aussi longtemps que votre compte
+          sera actif ou que cela sera nécessaire pour vous fournir le Service.
+          Nous conserverons et utiliserons également vos informations pour nous
+          conformer à nos obligations légales, régler les différends et
+          appliquer nos accords.
+        </p>
+
+        <h2 className="my-2 text-xl font-semibold text-primary-400">
+          Partage de vos informations
+        </h2>
+        <p>
+          Nous ne partageons pas vos informations personnelles avec des sociétés
+          tierces, sauf celles qui fournissent des services nécessaires au
+          fonctionnement du Service. Nous pouvons également divulguer vos
+          informations si la loi l&apos;exige ou pour protéger les droits et la
+          sécurité de nous-mêmes ou d&apos;autrui.
+        </p>
+
+        <h2>Vos choix</h2>
+        <p>Vous disposez de plusieurs choix concernant vos informations :</p>
+        <ul className="py-4">
+          <li>
+            1. Vous pouvez accéder et mettre à jour les informations de votre
+            compte à tout moment.
+          </li>
+          <li>
+            2. Vous pouvez choisir de vous désabonner de nos communications.
+          </li>
+          <li>
+            3. Vous pouvez demander la suppression de votre compte et de vos
+            informations.
+          </li>
+        </ul>
+        <h2 className="my-2 text-xl font-semibold text-primary-400">
+          Sécurité
+        </h2>
+        <p>
+          Nous prenons des mesures raisonnables pour protéger vos informations
+          contre tout accès, divulgation, altération ou destruction non
+          autorisés. Cependant, aucune transmission Internet ou stockage
+          électronique n&apos;est jamais totalement sécurisé. Nous ne pouvons
+          garantir la sécurité de vos informations.
+        </p>
+
+        <h2 className="my-2 text-xl font-semibold text-primary-400">
+          Confidentialité des enfants
+        </h2>
+        <p>
+          Notre Service ne s&apos;adresse à aucune personne de moins de 13 ans.
+          Nous ne collectons pas sciemment d&apos;informations personnelles de
+          personnes de moins de 13 ans. Si vous êtes un parent ou un tuteur et
+          que vous savez que votre enfant nous a fourni des informations
+          personnelles, veuillez nous contacter. Si nous apprenons que nous
+          avons collecté des informations personnelles d&apos;un enfant de moins
+          de 13 ans, nous prendrons des mesures pour supprimer ces informations
+          de nos serveurs.
+        </p>
+
+        <h2 className="my-2 text-xl font-semibold text-primary-400">
+          Modifications de la présente politique de confidentialité
+        </h2>
+        <p>
+          Nous pouvons mettre à jour notre politique de confidentialité de temps
+          à autre. Nous vous informerons de tout changement en publiant la
+          nouvelle politique de confidentialité sur cette page. Nous vous
+          encourageons à consulter régulièrement cette politique de
+          confidentialité pour prendre connaissance des mises à jour.
+        </p>
+        <h2 className="my-2 text-xl font-semibold text-primary-400">
+          Contactez-nous
+        </h2>
+        <p>
+          Si vous avez des questions concernant cette politique de
+          confidentialité, veuillez nous contacter par e-mail à{' '}
+          <b>admin@dugassistant.com</b> ou en visitant notre page de contact sur
+          le site Web.
+        </p>
+      </div>
+    </div>
+  );
+}
---
+++ src/pages/terms-usage.tsx
@@ -0,0 +1,183 @@
+import Head from 'next/head';
+import React from 'react';
+
+export default function TermsOfUsage() {
+  return (
+    <div className="text-justify">
+      <Head>
+        <title>Conditions d&apos;Utilisation de Dug Assistant</title>
+        <meta
+          name="description"
+          content="Conditions d'Utilisation de Dug Assistant"
+        />
+      </Head>
+      <div className="bg-primary-400 pb-8 pt-16 text-white ">
+        <div className="mx-auto max-w-[1000px] px-10">
+          <h1 className="mb-5 text-3xl font-bold text-white">
+            Conditions d&apos;Utilisation de Dug Assistant
+          </h1>
+        </div>
+      </div>
+      <div className="mx-auto max-w-[1000px] p-10">
+        <p className="mt-5">
+          En accédant ou en utilisant Dug Assistant (« Service »), fourni par
+          Dug Assistant (« nous », « notre », « nos »), vous acceptez
+          d&apos;être lié par ces Conditions d&apos;Utilisation (« Conditions
+          »). Si vous n&apos;acceptez pas ces Conditions, veuillez ne pas
+          utiliser le Service.
+        </p>
+
+        <h2 className="my-2 text-xl font-semibold text-primary-400">
+          Utilisation du Service
+        </h2>
+        <p>
+          Le Service est conçu pour aider les enseignants à générer des supports
+          pédagogiques. Vous acceptez d&apos;utiliser le Service uniquement à
+          des fins légales et conformément à ces Conditions. Vous êtes
+          responsable de la confidentialité de vos informations de compte et de
+          toutes les activités qui se produisent sous votre compte.
+        </p>
+
+        <h2 className="my-2 text-xl font-semibold text-primary-400">
+          Création de Compte
+        </h2>
+        <p>
+          Pour accéder à certaines fonctionnalités du Service, vous devrez
+          peut-être créer un compte. Lors de la création d&apos;un compte, vous
+          devez fournir des informations exactes et complètes, y compris votre
+          adresse e-mail, nom, et mot de passe. Vous êtes responsable de mettre
+          à jour ces informations au besoin pour les maintenir exactes.
+        </p>
+
+        <h2 className="my-2 text-xl font-semibold text-primary-400">
+          Paiement et Facturation
+        </h2>
+        <p>
+          Si vous choisissez d&apos;acheter des services via notre plateforme,
+          vous acceptez de fournir des informations de paiement précises et de
+          payer tous les frais associés aux services choisis. Les paiements
+          peuvent être traités via mobile money ou d&apos;autres méthodes de
+          paiement acceptées, et vos informations de paiement seront traitées
+          conformément à notre Politique de Confidentialité.
+        </p>
+
+        <h2 className="my-2 text-xl font-semibold text-primary-400">
+          Génération de Contenu
+        </h2>
+        <p>
+          Le Service vous permet de générer du contenu pédagogique en entrant
+          diverses données. Vous conservez la propriété du contenu que vous
+          créez en utilisant le Service. Cependant, vous nous accordez une
+          licence non exclusive, libre de redevances, mondiale pour utiliser,
+          modifier, et afficher ce contenu uniquement dans le cadre de
+          l&apos;exploitation du Service.
+        </p>
+
+        <h2 className="my-2 text-xl font-semibold text-primary-400">
+          Propriété Intellectuelle
+        </h2>
+        <p>
+          Tout le contenu et les matériaux disponibles sur le Service, y
+          compris, mais sans s&apos;y limiter, les textes, graphiques, logos, et
+          logiciels, sont la propriété intellectuelle de Dug Assistant ou de ses
+          concédants. Vous acceptez de ne pas reproduire, distribuer ou créer
+          des œuvres dérivées de tout contenu disponible sur le Service sans
+          notre autorisation écrite expresse.
+        </p>
+
+        <h2 className="my-2 text-xl font-semibold text-primary-400">
+          Confidentialité des Données
+        </h2>
+        <p>
+          Votre utilisation du Service est soumise à notre Politique de
+          Confidentialité, qui explique comment nous collectons, utilisons, et
+          protégeons vos informations personnelles. En utilisant le Service,
+          vous consentez à la collecte et à l&apos;utilisation de vos
+          informations comme décrit dans la Politique de Confidentialité.
+        </p>
+
+        <h2 className="my-2 text-xl font-semibold text-primary-400">
+          Activités Interdites
+        </h2>
+        <p>
+          Vous acceptez de ne pas utiliser le Service à des fins illégales ou
+          interdites, y compris, mais sans s&apos;y limiter :
+        </p>
+        <ul className="py-4">
+          <li>
+            Télécharger ou transmettre tout contenu nuisible, obscène ou
+            illégal.
+          </li>
+          <li>
+            Tenter d&apos;accéder de manière non autorisée au Service ou aux
+            comptes d&apos;autres utilisateurs.
+          </li>
+          <li>
+            s&apos;engager dans des activités pouvant nuire au Service, telles
+            que la distribution de virus ou de spam.
+          </li>
+        </ul>
+
+        <h2 className="my-2 text-xl font-semibold text-primary-400">
+          Résiliation
+        </h2>
+        <p>
+          Nous nous réservons le droit de suspendre ou de résilier votre compte
+          et l&apos;accès au Service à notre seule discrétion, sans préavis ni
+          responsabilité, si nous estimons que vous avez violé ces Conditions ou
+          que vous êtes autrement nuisible au Service ou à ses utilisateurs.
+        </p>
+
+        <h2 className="my-2 text-xl font-semibold text-primary-400">
+          Exclusion de Garantie
+        </h2>
+        <p>
+          Le Service est fourni &quot;tel quel&quot; et &quot;selon
+          disponibilité&quot;, sans aucune garantie d&apos;aucune sorte,
+          expresse ou implicite. Nous ne garantissons pas que le Service sera
+          ininterrompu, exempt d&apos;erreurs ou de composants nuisibles.
+        </p>
+
+        <h2 className="my-2 text-xl font-semibold text-primary-400">
+          Limitation de Responsabilité
+        </h2>
+        <p>
+          Dans toute la mesure permise par la loi, Dug Assistant ne sera pas
+          responsable des dommages directs, indirects, accidentels, spéciaux ou
+          consécutifs résultant de ou en relation avec votre utilisation du
+          Service.
+        </p>
+
+        <h2 className="my-2 text-xl font-semibold text-primary-400">
+          Modifications des Conditions
+        </h2>
+        <p>
+          Nous pouvons modifier ces Conditions de temps à autre. Toute
+          modification sera effective immédiatement après la publication des
+          Conditions mises à jour sur le Service. Votre utilisation continue du
+          Service après de telles modifications constitue votre acceptation des
+          nouvelles Conditions.
+        </p>
+
+        <h2 className="my-2 text-xl font-semibold text-primary-400">
+          Droit Applicable
+        </h2>
+        <p>
+          Ces Conditions sont régies et interprétées conformément aux lois de la
+          République Démocratique du Congo, sans égard à ses principes de
+          conflit de lois.
+        </p>
+
+        <h2 className="my-2 text-xl font-semibold text-primary-400">
+          Coordonnées
+        </h2>
+        <p>
+          Si vous avez des questions ou des préoccupations concernant ces
+          Conditions, veuillez nous contacter à{' '}
+          <a href="mailto:admin@dugassistant.com">admin@dugassistant.com</a> ou
+          via la page de contact de notre site Web.
+        </p>
+      </div>
+    </div>
+  );
+}
