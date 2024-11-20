cc423e5c36952478427b717a36ddd8f2b2c8a9ea
Ciragane Nicole <ciraganenicole@gmail.com>
2024-11-20 11:30:41 -0800

    Revert "Hoppy hoppy2"

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,35 +1,5 @@
-import axios from 'axios';
-
 import { Base } from '../templates/Base';
 
-const Index = (props: any) => <Base {...props} />;
-
-export async function getServerSideProps() {
-  try {
-    const res = await axios.get(
-      `${process.env.APP_LINK}/api/photo?folder=dugassistant`,
-    );
-
-    const photos = res.data.photos.map((photo: any) => ({
-      src: photo.url.replace('/upload/', '/upload/w_1080,f_auto/'),
-      width: 1080,
-      height: photo.height / (photo.width / 1080),
-    }));
-
-    return {
-      props: {
-        photos,
-        error: null,
-      },
-    };
-  } catch (error) {
-    return {
-      props: {
-        photos: [],
-        error: 'Failed to fetch photos',
-      },
-    };
-  }
-}
+const Index = () => <Base />;
 
 export default Index;
