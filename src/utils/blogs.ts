113b142e0e18c1f4d0ca359ff34a7d32e5749470
ciraganenicole <ciraganenicole@gmail.com>
2024-11-24 05:27:08 -0800

    Fixes

--- src/utils/blogs.ts
+++ src/utils/blogs.ts
@@ -1,13 +1,57 @@
 export const blogs = [
   {
     id: '1',
+    image: '/assets/blog/article2.jpeg',
+    author: 'Ciragane Nicole',
+    date: '1 Nov 2024',
+    title: "Réussir la Transition vers l'APC/APS en RDC",
+    tags: ['Education', 'Intelligence artificielle'],
+    link: 'https://medium.com/@harvelyc/r%C3%A9ussir-la-transition-vers-lapc-aps-en-rdc-avec-dugassistant-b6cfec747ebd',
+    description:
+      'La mise en œuvre du système éducatif basé sur les Approches par Compétences et par Situation ...',
+  },
+  {
+    id: '2',
     image: '/assets/blog/article1.jpeg',
     author: 'Ciragane Nicole',
-    date: '24 Nov 2024',
+    date: '5 Nov 2024',
     title: 'L’impact de DugAssistant sur les enseignantes',
-    tags: ['Femme', 'Education'],
-    link: 'https://medium.com/p/3b2157790e8e/edit',
+    tags: ['Femmes', 'Education'],
+    link: 'https://medium.com/@harvelyc/limpact-de-dugassistant-sur-les-enseignantes-un-soutien-essentiel-pour-concilier-travail-et-vie-97e1b8d7bfd9',
     description:
-      'Les enseignantes jouent un rôle clé dans le système éducatif en RDC.',
+      'Les enseignantes jouent un rôle clé dans le système éducatif en République Démocratique du Congo.',
+  },
+  {
+    id: '3',
+    image: '/assets/blog/article3.jpeg',
+    author: 'Ciragane Nicole',
+    date: '12 Nov 2024',
+    title: "L'Influence de DugAssistant sur les Élèves",
+    tags: ['Enfants', 'Education', 'IA'],
+    link: 'https://medium.com/@harvelyc/linfluence-de-dugassistant-sur-l-apprentissage-des-%C3%A9l%C3%A8ves-da4a05a27e51',
+    description:
+      'Les élèves, souvent confrontés à des ressources limitées, ont besoin d’une approche éducative plus interactive',
+  },
+  {
+    id: '4',
+    image: '/assets/blog/article4.png',
+    author: 'Ciragane Nicole',
+    date: '17 Nov 2024',
+    title: "L'Éducation et le Futur du Travail",
+    tags: ['Travail', 'Education', 'Technologie'],
+    link: 'https://medium.com/@harvelyc/l%C3%A9ducation-et-le-futur-du-travail-comment-dugassistant-pr%C3%A9pare-les-%C3%A9l%C3%A8ves-congolais-aux-c44612876e71',
+    description:
+      'DugAssistant Prépare les Élèves Congolais aux Compétences du 21e Siècle',
+  },
+  {
+    id: '5',
+    image: '/assets/blog/article5.png',
+    author: 'Ciragane Nicole',
+    date: '22 Nov 2024',
+    title:
+      'DugAssistant et la Révolution Numérique dans les Écoles Congolaises',
+    tags: ['RDC', 'Education', 'IA'],
+    link: 'https://medium.com/@harvelyc/dugassistant-et-la-r%C3%A9volution-num%C3%A9rique-dans-les-%C3%A9coles-congolaises-39dc1d27f309',
+    description: 'L’éducation en RDC fait face à de nombreux défis',
   },
 ];
